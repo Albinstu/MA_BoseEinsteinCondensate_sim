e488e98c1479572f2609f358642f86f2277fd89d
Albin Sturesson <alb.stu@gmail.com>
2026-03-12 14:28:22 +0100

    update Besse sim on road to parallel

--- BEC_sim_besse.py
+++ BEC_sim_besse.py
@@ -7,10 +7,10 @@ Created on Thu Feb 26 10:39:22 2026
 """
 
 import numpy as np
-from dolfinx import fem, mesh, plot, default_scalar_type
+from dolfinx import fem, mesh, plot, default_scalar_type, io
 from mpi4py import MPI # for creating mesh
 from petsc4py import PETSc
-from dolfinx.fem.petsc import LinearProblem #, NonlinearProblem
+from dolfinx.fem.petsc import LinearProblem
 from dolfinx.io import gmsh as gmshio
 import gmsh # mesh
 import ufl
@@ -38,21 +38,20 @@ else:
     exit() # not correct way to handle
 
 ## DOMAIN PARAMETERS
-# box_width_comp_domain = 16 # of computational domain
-# box_height_comp_domain = 9 # -- || --
-box_width_comp_domain = 16 # of computational domain
-box_height_comp_domain = 10 # -- || --
+box_width_comp_domain = 12 # of computational domain
+box_height_comp_domain = 12 # -- || --
 
 # rectangular domain is centered at origo
 a = box_width_comp_domain * 0.5 # half axis of box width
 b = box_height_comp_domain * 0.5 # half axis of box height
-delta = 1 # change to larger maybe 1 or 2
+delta = 2 # change to larger maybe 1 or 2
 
 # potential coordinates; row 0 - potential x-coord bounds
 #                        row 1 - potential y-coord bounds
 potential_coords = [(box_width_comp_domain * 0.25 - a, 
-                    box_width_comp_domain * 0.25 - a 
-                    + 2.5/8 * box_width_comp_domain), 
+                    box_width_comp_domain * 0.25 - a \
+                    # + 2.5/8 * box_width_comp_domain), 
+                    + 3), 
                    (-b, b)]
 
 # computational_domain_coord_ranges = [(-a, a),
@@ -71,17 +70,12 @@ ext_domain_polygon_vertices = [(-a - delta, -b - delta),
 
 ## EQUATION COEFFICIENT PARAMETERS
 u = 2 # velocity of coupled term
-g = 0.8 # scattering amplitude 
+g = 0.25 # scattering amplitude 
 g1 = g # IF CHANGE VALUE HERE, MUST CHAGNE VALUE IN NON-LINEAR FUNC
 g2 = g
 g3 = g
 # b1 = np.array([1, -1j]) # convection vectors
 # b2 = np.array([1, 1j]) # ---- || ----
-
-T = 1 # end time
-N = 500 # number of time steps from 0th
-dt = T / N # time step
-dt_inv = N / T
 
 
 # radius of start circ
@@ -94,30 +88,18 @@ initial_coord = np.array([- start_circ_r * np.cos(ang_of_atac),
 
 # momentum vector of initial cond
 # p_vec = - 20 * initial_coord[:] / np.linalg.norm(initial_coord)
-p_vec = - 5 * (initial_coord[:] / np.linalg.norm(initial_coord))
+p_vec = + 6 * (initial_coord[:] / np.linalg.norm(initial_coord))
 # p_vec = [1, 0] # velocity of initial cond
 
 gauss_width = 1 # arbitrarily chosen # smaller diffuses faster
 state_pop1 = 1 / np.sqrt(2) # state population
-# state_pop1 = 1 # state population
-state_pop2 = state_pop1
+state_pop2 = - state_pop1
 
 
 
 # =============================================================================
 # FUNCTIONS
 # =============================================================================
-def initial_condition(x, state_pop, init_coord=initial_coord, p_vec=p_vec):
-    consts = state_pop / ( gauss_width * np.sqrt(np.pi))
-    gauss_pack = np.exp(- ( (x[0] - init_coord[0])**2 \
-       + (x[1] - init_coord[1])**2 ) / (2 * gauss_width ** 2) )
-    
-    wave_prop = np.exp( 1j * ( p_vec[0] * (x[0] - init_coord[0]) \
-                            + p_vec[1] * (x[1] - init_coord[1]) ))
-    
-    return consts * gauss_pack * wave_prop
-
-
 def plot_mesh(mesh: mesh.Mesh, values=None):
     """
     Given a DOLFINx mesh, create a pyvista.UnstructuredGrid,
@@ -211,11 +193,103 @@ def polygon_mesh(vertices, h):
     return meshdata.mesh, meshdata.cell_tags, meshdata.facet_tags
 
 
+def plotter_func(domain_mesh, sim_solutions, sim_time_iter_var):
+    # UNPACK variables
+    T = sim_time_iter_var[0]
+    N_iter = sim_time_iter_var[1]
+    dt = sim_time_iter_var[2]
+    dt_inv = sim_time_iter_var[3]
+    
+    # if not same length then something wrong
+    assert (N_iter + 1) == (sim_solutions.shape[0] // 2)
+    
+    
+    V = fem.functionspace(domain_mesh, ("Lagrange", 1))
+    
+        
+    psi1_t = sim_solutions[0]
+    psi2_t = sim_solutions[N_iter+1]
+    
+    
+    ########### PLOTTER SETUP ############
+    grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V))
+    pyvista.OFF_SCREEN = True
+    
+    plotter = pyvista.Plotter()
+    plotter.window_size = [2048, 1536] # doubles window size
+    plotter.open_gif("PSI_evolution.gif", fps=N // 10)
+    
+    grid.point_data["Psi"] = np.abs(psi1_t)**2 \
+        + np.abs(psi2_t)**2
+    # warped = grid.warp_by_scalar("Psi", factor=1)
+    
+    viridis = plt.colormaps.get_cmap("viridis").resampled(1000)
+    sargs = dict(
+        title_font_size=25,
+        label_font_size=20,
+        fmt="%.2e",
+        color="black",
+        position_x=0.9,
+        position_y=0.1,
+        width=0.1,
+        height=0.8,
+        vertical=True
+    )
+    
+    renderer = plotter.add_mesh(
+        # warped,
+        grid,
+        # show_edges=True,
+        show_edges=False,
+        lighting=False,
+        cmap=viridis,
+        scalar_bar_args=sargs,
+        clim=[0, 
+              max(np.abs(psi1_t)**2 + np.abs(psi2_t)**2)
+              ],
+    )
+    
+    plotter.view_xy()
+    plotter.camera.zoom(1.38)
+        
+    for it in range(N_iter+1):
+        psi1_t = sim_solutions[it]
+        psi2_t = sim_solutions[(N_iter+1) + it]
+        
+        prob = np.abs(psi1_t)**2 + np.abs(psi2_t)**2
+        
+        grid.point_data["Psi"][:] = prob
+        
+        plotter.write_frame()
+        # if not it % (N//100) :
+        #     break
+    
+    plotter.close()
+    # pyvista.close_all()
+    
+    return None
+
+
+def initial_condition(x, state_pop, gauss_width=gauss_width,
+                      init_coord=initial_coord, mom_vec=p_vec):
+    px = mom_vec[0]
+    py = mom_vec[1]
+    
+    consts = state_pop / ( gauss_width * np.sqrt(np.pi))
+    gauss_pack = np.exp(- ( (x[0] - init_coord[0])**2 \
+       + (x[1] - init_coord[1])**2 ) / (2 * gauss_width ** 2) )
+    
+    wave_prop = np.exp( 1j * ( px * (x[0] - init_coord[0]) \
+                            + py * (x[1] - init_coord[1]) ))
+    
+    return consts * gauss_pack * wave_prop
+
+
 def CAP_func(x): # Complex absorbing potential
     abs_x = np.abs(x[0]) # - a to optimise
     abs_y = np.abs(x[1]) # - b to optimise
-    n = 2
-    CAP_strength = 50
+    n = 4
+    CAP_strength = 5
     index_func_x = (abs_x > a) # gep ?
     index_func_y = (abs_y > b) # geq ?
     inv_delta = 1 / delta
@@ -250,218 +324,231 @@ def F_nonlin_func(psi1_arr, psi2_arr):
     return p1 + p2
 
 
-def problem_solver(domain_mesh):
-    pass
+def L2_norm_printer(f1, f2):
+    # takes two fem.Function arguments
+    comm = f1.function_space.mesh.comm
+    PSI_norm = fem.form(( f1 * ufl.conj( f1 ) + f2 * ufl.conj( f2 ))* ufl.dx)
+    PSI_norm_glob = np.sqrt(comm.allreduce(
+        fem.assemble_scalar(PSI_norm), 
+        MPI.SUM)
+        )
     
-
-
-# =============================================================================
-# SOLVER CODE
-# =============================================================================
-ext_domain_mesh, _, _ = polygon_mesh(ext_domain_polygon_vertices, 0.1)
-# plot_mesh(ext_domain_mesh)
-
-
-
-# DISCRETE SPACES
-# function space
-V = fem.functionspace(ext_domain_mesh, ("Lagrange", 1)) 
-# coeff func space; one-to-one connection cell and value
-Q = fem.functionspace(ext_domain_mesh, ("DG", 0)) 
-
-
-# DEFINE COEFF
-CAP_W = fem.Function(Q)
-CAP_W.name = "CAP = W"
-CAP_W.interpolate(CAP_func)
-
-pot_V = fem.Function(Q)
-pot_V.name = "V"
-pot_V.interpolate(potential_func)
-
-
-# APPLY BC
-tdim = ext_domain_mesh.topology.dim # give dim of cells
-fdim = tdim - 1 # dim of facets/edges
-ext_domain_mesh.topology.create_connectivity(fdim, tdim) # edge to cell
-boundary_facets = mesh.exterior_facet_indices(ext_domain_mesh.topology) 
-boundary_dofs = fem.locate_dofs_topological(V, fdim, boundary_facets)
-bcs = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
-
-
-# CONSTRUCT FUNCTIONS
-psi1_old = fem.Function(V)
-psi2_old = fem.Function(V)
-
-psi1_new = fem.Function(V)
-psi2_new = fem.Function(V)
-
-# steppers part of relaxation scheme
-gamma_new = fem.Function(V)
-phi1_new = fem.Function(V)
-phi2_new = fem.Function(V)
-
-
-# trial & test function
-PHI = ufl.TrialFunction(V)
-v = ufl.TestFunction(V)
-v_conj = ufl.conj(v) # conjugate needed instead of ufl.inner
-
-
-b1_vec = ufl.as_tensor([1.0 + 0.0j, -1.0j, 0.0j]) # WORKS AND IS CLEAN
-b2_vec = ufl.as_tensor([1.0 + 0.0j, 1.0j, 0.0j]) # WORKS AND IS CLEAN
-
-
-
-
-# create non_linear func
-# IF VALUE OF g1,g2,g3 IS CHANGED THEN MUST REWRITE FUNCTIONS
-# F_nonlin_func = lambda psi1, psi2 : ufl.conj(psi1) * g * ( psi1 + psi2 ) +\
-#     ufl.conj(psi2) * g * ( psi1 + psi2 )
-
-
-# INIT
-psi1_old.interpolate(lambda x : initial_condition(x, state_pop1))
-psi2_old.interpolate(lambda x : initial_condition(x, state_pop2))
-
-
-gamma_new.x.array[:] = F_nonlin_func(psi1_old.x.array[:], psi2_old.x.array[:])
-phi1_new.x.array[:] = psi2_old.x.array[:]
-phi2_new.x.array[:] = psi1_old.x.array[:]
-
-
-
-# CONSTRUCT BILINEAR AND LINEAR FORMS
-a1_const = (j_im * 2 * dt_inv - pot_V + j_im * CAP_W) * PHI * v_conj * ufl.dx
-a1_const += - 0.5 * ufl.inner( ufl.grad(PHI), ufl.grad(v)) * ufl.dx
-a1 = - gamma_new * PHI * v_conj * ufl.dx + a1_const
-
-L1 = j_im * 4 * dt_inv * psi1_old * v_conj * ufl.dx
-L1 += - j_im * u * ufl.dot(b1_vec, ufl.grad(phi1_new + psi2_old)) * v_conj * ufl.dx
-
-prob1 = LinearProblem(
-    a1,
-    L1,
-    bcs=[bcs],
-    u=psi1_new,
-    petsc_options={"ksp_type": "preonly", "pc_type": "lu"}, 
-    # petsc_options = { # best for medium 
-    # "ksp_type": "gmres",
-    # "pc_type": "ilu"
-    #             },
-    # petsc_options = { # best for large parallel by chatgpt "FEniCSx complex conjugate..."
-    # "ksp_type": "gmres",
-    # "pc_type": "hypre",
-    # "pc_hypre_type": "boomeramg"
-    # },
-    petsc_options_prefix="psi1_solver_",
-    )
-
-
-
-
-a2_const = (j_im * 2 * dt_inv - pot_V + j_im * CAP_W) * PHI * v_conj * ufl.dx
-a2_const += - 0.5 * ufl.inner( ufl.grad(PHI), ufl.grad(v)) * ufl.dx
-a2 = - gamma_new * PHI * v_conj * ufl.dx + a2_const
-
-L2 = j_im * 4 * dt_inv * psi2_old * v_conj * ufl.dx
-L2 += - j_im * u * ufl.dot(b2_vec, ufl.grad(phi2_new + psi1_old)) * v_conj * ufl.dx
-
-prob2 = LinearProblem(
-    a2,
-    L2,
-    bcs=[bcs],
-    u=psi2_new,
-    petsc_options={"ksp_type": "preonly", "pc_type": "lu"}, 
-    # petsc_options = { # best for medium 
-    # "ksp_type": "gmres",
-    # "pc_type": "ilu"
-    #             },
-    # petsc_options = { # best for large parallel by chatgpt "FEniCSx complex conjugate..."
-    # "ksp_type": "gmres",
-    # "pc_type": "hypre",
-    # "pc_hypre_type": "boomeramg"
-    # },
-    petsc_options_prefix="psi1_solver_",
-    )
-
-
-########### PLOTTER SETUP ############
-grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V))
-
-plotter = pyvista.Plotter()
-plotter.open_gif("u_time.gif", fps=10)
-
-grid.point_data["Psi"] = np.abs(psi1_new.x.array)**2 \
-    + np.abs(psi1_new.x.array)**2
-# warped = grid.warp_by_scalar("Psi", factor=1)
-
-viridis = plt.colormaps.get_cmap("viridis").resampled(1000)
-sargs = dict(
-    title_font_size=25,
-    label_font_size=20,
-    fmt="%.2e",
-    color="black",
-    position_x=0.1,
-    position_y=0.8,
-    width=0.8,
-    height=0.1,
-)
-
-renderer = plotter.add_mesh(
-    # warped,
-    grid,
-    # show_edges=True,
-    show_edges=False,
-    lighting=False,
-    cmap=viridis,
-    scalar_bar_args=sargs,
-    clim=[0, max(np.abs(psi1_new.x.array)**2 + np.abs(psi2_new.x.array)**2)],
-)
-plotter.view_xy()
-# plotter.camera.tight()
-plotter.camera.zoom(1.2)
-
-
-for _ in range(N//2):
+    # if procc == 0:
+    print(PSI_norm_glob)
     
-    # solve problem
-    prob1.solve()
-    prob2.solve()
+    return None
+
+
+def problem_solver(domain_mesh, time_iter_var : list,):
     
-    # update solutions
-    psi1_new.x.array[:] = psi1_new.x.array[:] - psi1_old.x.array[:]
-    psi1_old.x.array[:] = psi1_new.x.array[:]
-    
-    psi2_new.x.array[:] = psi2_new.x.array[:] - psi2_old.x.array[:]
-    psi2_old.x.array[:] = psi2_new.x.array[:]
+    # UNPACK variables for time stepper
+    # T = time_iter_var[0]
+    N_iter = time_iter_var[1]
+    # dt = time_iter_var[2]
+    dt_inv = time_iter_var[3]
     
     
-    gamma_new.x.array[:] = - gamma_new.x.array
-    gamma_new.x.array[:] += 2*F_nonlin_func(psi1_old.x.array[:], psi2_old.x.array[:])
+    # DISCRETE SPACES
+    # function space
+    V = fem.functionspace(domain_mesh, ("Lagrange", 1))
+    # coeff func space; one-to-one connection cell and value
+    Q = fem.functionspace(domain_mesh, ("DG", 0)) 
     
-    phi1_new.x.array[:] = 2 * psi2_old.x.array[:] - phi1_new.x.array[:]
-    phi2_new.x.array[:] = 2 * psi1_old.x.array[:] - phi2_new.x.array[:]
-
-    prob = np.abs(psi1_new.x.array)**2 + np.abs(psi2_new.x.array)**2
     
-    # new_warped = grid.warp_by_scalar("Psi", factor=1)
-    # warped.points[:, :] = new_warped.points
-    # warped.point_data["Psi"][:] = prob
+    # DEFINE COEFF
+    CAP_W = fem.Function(Q)
+    CAP_W.name = "CAP = W"
+    CAP_W.interpolate(CAP_func)
     
-    grid.point_data["Psi"][:] = prob
+    pot_V = fem.Function(Q)
+    pot_V.name = "V"
+    pot_V.interpolate(potential_func)
     
-    plotter.write_frame()
+    
+    # APPLY BC
+    tdim = domain_mesh.topology.dim # give dim of cells
+    fdim = tdim - 1 # dim of facets/edges
+    domain_mesh.topology.create_connectivity(fdim, tdim) # edge to cell
+    boundary_facets = mesh.exterior_facet_indices(domain_mesh.topology) 
+    boundary_dofs = fem.locate_dofs_topological(V, fdim, boundary_facets)
+    bcs = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
+    
+    
+    # CONSTRUCT FUNCTIONS
+    psi1_old = fem.Function(V)
+    psi2_old = fem.Function(V)
+    
+    psi1_new = fem.Function(V)
+    psi2_new = fem.Function(V)
+    
+    # steppers part of relaxation scheme
+    gamma_new = fem.Function(V)
+    phi1_new = fem.Function(V)
+    phi2_new = fem.Function(V)
+    
+    
+    # trial & test function
+    PHI = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    v_conj = ufl.conj(v) # conjugate needed instead of ufl.inner
+    
+    
+    b1_vec = ufl.as_vector([1.0 + 0.0j, -1.0j, 0.0j]) # WORKS AND IS CLEAN
+    b2_vec = ufl.as_vector([1.0 + 0.0j, 1.0j, 0.0j]) # WORKS AND IS CLEAN
+    
+    
+    # INIT
+    psi1_old.interpolate(lambda x : initial_condition(x, state_pop1))
+    psi2_old.interpolate(lambda x : initial_condition(x, state_pop2))
+    
+    
+    gamma_new.x.array[:] = F_nonlin_func(psi1_old.x.array[:], psi2_old.x.array[:])
+    phi1_new.x.array[:] = psi2_old.x.array[:]
+    phi2_new.x.array[:] = psi1_old.x.array[:]
+    
+    
+    
+    # CONSTRUCT BILINEAR AND LINEAR FORMS
+    a_const = (j_im * 2 * dt_inv - pot_V + j_im * CAP_W) * PHI * v_conj * ufl.dx
+    a_const += - 0.5 * ufl.inner( ufl.grad(PHI), ufl.grad(v) ) * ufl.dx
+    a = - gamma_new * PHI * v_conj * ufl.dx + a_const
+    
+    
+    L1 = j_im * 4 * dt_inv * psi1_old * v_conj * ufl.dx
+    L1 += - j_im * u * ufl.dot(b1_vec, ufl.grad(phi1_new + psi2_old)) * v_conj * ufl.dx
+    
+    
+    L2 = j_im * 4 * dt_inv * psi2_old * v_conj * ufl.dx
+    L2 += - j_im * u * ufl.dot(b2_vec, ufl.grad(phi2_new + psi1_old)) * v_conj * ufl.dx
+    
+    
+    prob1 = LinearProblem(
+        a,
+        L1,
+        bcs=[bcs],
+        u=psi1_new,
+        # petsc_options={"ksp_type": "preonly", "pc_type": "lu"}, 
+        petsc_options = { # best for medium 
+        "ksp_type": "gmres",
+        "pc_type": "ilu"
+                    },
+        # petsc_options = { # best for large parallel by chatgpt "FEniCSx complex conjugate..."
+        # "ksp_type": "gmres",
+        # "pc_type": "hypre",
+        # "pc_hypre_type": "boomeramg"
+        # },
+        petsc_options_prefix="psi1_solver_",
+        )
+    
+    
+    prob2 = LinearProblem(
+        a,
+        L2,
+        bcs=[bcs],
+        u=psi2_new,
+        # petsc_options={"ksp_type": "preonly", "pc_type": "lu"}, 
+        petsc_options = { # best for medium 
+        "ksp_type": "gmres",
+        "pc_type": "ilu"
+                    },
+        # petsc_options = { # best for large parallel by chatgpt "FEniCSx complex conjugate..."
+        # "ksp_type": "gmres",
+        # "pc_type": "hypre",
+        # "pc_hypre_type": "boomeramg"
+        # },
+        petsc_options_prefix="psi2_solver_",
+        )
+    
+    
+    
+    
+    complete_sim_solutions = np.zeros(
+        ( 2 * (N_iter+1), psi1_old.x.array.shape[0] ),
+        dtype=np.complex128()
+        )
+    
+    complete_sim_solutions[0] = psi1_old.x.array[:]
+    complete_sim_solutions[N_iter+1] = psi2_old.x.array[:]
 
-plotter.close()
+    
+    # L2 norm
+    L2_norm_printer(psi1_old, psi2_old)
+    
+    
+    for it in range(1, N_iter+1):
+        
+        # solve problem
+        prob1.solve()
+        prob2.solve()
+        
+        # update solutions
+        # psi1_new.x.array[:] = psi1_new.x.array[:] - psi1_old.x.array[:]
+        psi1_old.x.array[:] = psi1_new.x.array[:] - psi1_old.x.array[:]
+        
+        # psi2_new.x.array[:] = psi2_new.x.array[:] - psi2_old.x.array[:]
+        psi2_old.x.array[:] = psi2_new.x.array[:] - psi2_old.x.array[:]
+        
+        
+        gamma_new.x.array[:] = - gamma_new.x.array[:]
+        gamma_new.x.array[:] += 2*F_nonlin_func(
+            psi1_old.x.array[:], 
+            psi2_old.x.array[:]
+            )
+        
+        # force update the rhs
+        # prob1.A.zeroEntries()
+        prob1.A.assemble()
+        
+        # prob2.A.zeroEntries()
+        prob2.A.assemble()
+        
+        
+        phi1_new.x.array[:] = 2 * psi2_old.x.array[:] - phi1_new.x.array[:]
+        phi2_new.x.array[:] = 2 * psi1_old.x.array[:] - phi2_new.x.array[:]
+        
+        
+        # store solution
+        complete_sim_solutions[it] = psi1_old.x.array[:]
+        complete_sim_solutions[(N_iter+1)+it] = psi2_old.x.array[:]
+        
+        # if it % (N // 10):
+        #     break
+
+    
+    # L2 norm
+    L2_norm_printer(psi1_old, psi2_old)
+    
+    # # comm = psi1_new.function_space.mesh.comm
+    # PSI_norm = fem.form((psi1_new * ufl.conj(psi1_new) \
+    #                      + psi2_new * ufl.conj(psi2_new))* ufl.dx)
+    # PSI_norm_glob = np.sqrt(comm.allreduce(fem.assemble_scalar(PSI_norm), MPI.SUM))
+    # print(PSI_norm_glob)
+    
+    return complete_sim_solutions
 
 
-# L2 norm
-comm = psi1_new.function_space.mesh.comm
-PSI_norm = fem.form((psi1_new * ufl.conj(psi1_new) \
-                     + psi2_new * ufl.conj(psi2_new))* ufl.dx)
-PSI_norm_glob = np.sqrt(comm.allreduce(fem.assemble_scalar(PSI_norm), MPI.SUM))
-print(PSI_norm_glob)
+
+T = 1 # end time
+N = 300 # number of time steps from 0th
+dt = T / N # time step
+dt_inv = N / T
+sim_time_iter_var = [T, N // 2, dt, dt_inv] 
+
+h = 0.09 # max mesh size
+ext_domain_mesh, _, _ = polygon_mesh(ext_domain_polygon_vertices, h)
+
+
+
+sim_solutions = problem_solver(ext_domain_mesh, sim_time_iter_var)
+
+
+plotter_func(ext_domain_mesh, sim_solutions, sim_time_iter_var)
+
+
+
+
+
+
 
 
 # =============================================================================
